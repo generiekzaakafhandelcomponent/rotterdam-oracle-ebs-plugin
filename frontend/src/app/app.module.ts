8effe550e7c0acb952452dc7f71aecb21a78564e
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2026-04-21 12:30:22 +0200

    Fix app.module.ts: use RotterdamOracleEbsPluginModule and rotterdamOracleEbsPluginSpecification

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -98,7 +98,7 @@ import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
 import {environment} from "../environments/environment";
 import {registerDocumentenApiFormioUploadComponent, ZgwModule} from "@valtimo/zgw";
 
-import {SamplePluginModule, samplePluginSpecification,} from "@valtimo-plugins/rotterdam-oracle-ebs";
+import {RotterdamOracleEbsPluginModule, rotterdamOracleEbsPluginSpecification,} from "@valtimo-plugins/rotterdam-oracle-ebs";
 
 export function tabsFactory() {
   return new Map<string, object>([
@@ -158,7 +158,7 @@ export function tabsFactory() {
     ProcessModule,
     ReactiveFormsModule,
     ResourceModule,
-    SamplePluginModule,
+    RotterdamOracleEbsPluginModule,
     SecurityModule,
     SseModule,
     SwaggerModule,
@@ -190,7 +190,7 @@ export function tabsFactory() {
         objecttypenApiPluginSpecification,
         openNotificatiesPluginSpecification,
         openZaakPluginSpecification,
-        samplePluginSpecification,
+        rotterdamOracleEbsPluginSpecification,
         zakenApiPluginSpecification,
       ],
     },
